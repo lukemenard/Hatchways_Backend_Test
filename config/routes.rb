b4728ce8fde66870571b14a29d764c335149e2a1
lukemenard <lukemenard@gmail.com>
2019-11-06 14:06:06 -0700

    defined the v2 namespace in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  scope module: :v2, constraints: ApiVersion.new('v2') do
+    resources :todos, only: :index
+  end
+
   scope module: :v1, constraints: ApiVersion.new('v1', true) do
     resources :todos do
       resources :items
